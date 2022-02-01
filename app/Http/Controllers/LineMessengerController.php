9996857005dd4f5bb3a3836947fc44fc27e836d5
azumiyu <azumiyukihiro517@icloud.com>
2022-02-01 09:33:38 +0900

    LINE API 編集

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -33,7 +33,43 @@ class LineMessengerController extends Controller
             // ユーザーにメッセージを返す
             $reply=$bot->replyText($reply_token, $reply_message);
             
+            // LINEのユーザーIDをuserIdに代入
+            $userId=$request['events'][0]['source']['userId'];
+ 
+            // userIdがあるユーザーを検索
+            $user=User::where('line_id', $userId)->first();
+ 
+            // もし見つからない場合は、データベースに保存
+            if($user==NULL) {
+                $profile=$bot->getProfile($userId)->getJSONDecodedBody();
+ 
+                $user=new User();
+                $user->provider='line';
+                $user->line_id=$userId;
+                $user->name=$profile['displayName'];
+                $user->save();
+            }
+            
             return 'ok';
         }
+    }
+    
+    // メッセージ送信用
+    public function message() {
+ 
+        // LINEBOTSDKの設定
+        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
+        $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+ 
+        // LINEユーザーID指定
+        $userId = "LINEユーザーID";
+ 
+        // メッセージ設定
+        $message = "こんにちは！";
+ 
+        // メッセージ送信
+        $textMessageBuilder = new TextMessageBuilder($message);
+        $response    = $bot->pushMessage($userId, $textMessageBuilder);
+ 
     }
 }
