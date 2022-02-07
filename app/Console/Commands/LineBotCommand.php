d58e6d140b611959ff67bc72bed3e86a0dd7e991
azumiyu <azumiyukihiro517@icloud.com>
2022-02-07 09:47:57 +0900

    Line APIテスト

--- app/Console/Commands/LineBotCommand.php
+++ app/Console/Commands/LineBotCommand.php
@@ -3,6 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot;
+use App\Models\User;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use Illuminate\Http\Request;
 
 class LineBotCommand extends Command
 {
@@ -35,8 +40,19 @@ class LineBotCommand extends Command
      *
      * @return mixed
      */
-    public function handle()
+    public function handle(Request $request)
     {
-        //
+        $reply_token=$inputs['events'][0]['replyToken'];
+ 
+        // LINEBOTSDKの設定
+        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
+        $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+ 
+        // 送信するメッセージの設定
+        $reply_message='メッセージありがとうございます';
+ 
+        // ユーザーにメッセージを返す
+        $reply=$bot->replyText($reply_token, $reply_message);
+        return 'ok';
     }
 }
