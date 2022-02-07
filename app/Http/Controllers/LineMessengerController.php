2f567ab56734ef074994d7acd3df5e5921f30929
azumiyu <azumiyukihiro517@icloud.com>
2022-02-07 14:49:59 +0900

    Line API テスト

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot;
-use App\Models\User;
+use App\User;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 use Illuminate\Http\Request;
