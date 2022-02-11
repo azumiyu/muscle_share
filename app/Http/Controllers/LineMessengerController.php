62314663b262560f0e8ed6468e5889c283402b05
azumiyu <azumiyukihiro517@icloud.com>
2022-02-11 22:17:49 +0900

    LineAPI メッセージ追加

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -107,6 +107,12 @@ class LineMessengerController extends Controller
             $rank = $i + 1;
             $squat = $rank."位 ".$name2[$i]." ".$weight2[$i]."kg ".$rep2[$i]."回".PHP_EOL;
         }
+        if ($squat = NULL) {
+            $squat = "今月のスクワットのデータはありません";
+        }
+        if ($bench = NULL) {
+            $bench = "今月のスクワットのデータはありません";
+        }
         
         $message="今月のランキング！
         ベンチプレス→".PHP_EOL.
